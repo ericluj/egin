a7a98506fa54a036f30543ba1648a1d4adab6d55
ericluj <847751875@qq.com>
2022-09-19 20:40:39 +0800

    tree

--- tree.go
+++ tree.go
@@ -37,18 +37,100 @@ const (
 )
 
 type node struct {
-	path      string
-	indices   string
+	// 结点路径
+	path string
+	// 分支首字母拼接而成
+	indices string
+	// 结点是否是参数结点
 	wildChild bool
-	nType     nodeType
-	priority  uint32
-	children  []*node
-	handlers  HandlersChain
-	fullPath  string
+	// 结点类型
+	nType nodeType
+	// 优先级（子结点等注册的handler数量）
+	priority uint32
+	// 子结点
+	children []*node
+	// 处理函数链
+	handlers HandlersChain
+	// 完整路径
+	fullPath string
+}
+
+func min(a, b int) int {
+	if a <= b {
+		return a
+	}
+	return b
+}
+
+func longestCommonPrefix(a, b string) int {
+	i := 0
+	max := min(len(a), len(b))
+	for i < max && a[i] == b[i] {
+		i++
+	}
+	return i
 }
 
 func (n *node) addRoute(path string, handlers HandlersChain) {
+	fullPath := path
+	n.priority++
 
+	// 空树
+	if len(n.path) == 0 && len(n.children) == 0 {
+		n.insertChild(path, fullPath, handlers)
+		n.nType = root
+		return
+	}
+
+	parentFullPathIndex := 0
+
+walk:
+	for {
+		// 获取公共前缀
+		i := longestCommonPrefix(path, n.path)
+
+		// 当前结点的一部分为公共前缀，将其后的部分分裂出来作为新的子结点
+		if i < len(n.path) {
+			child := node{
+				path:      n.path[i:],
+				wildChild: n.wildChild,
+				indices:   n.indices,
+				children:  n.children,
+				handlers:  n.handlers,
+				priority:  n.priority - 1,
+				fullPath:  n.fullPath,
+			}
+			n.children = []*node{&child}
+			n.indices = bytesconv.BytesToString([]byte{n.path[i]})
+			n.path = path[:i]
+			n.handlers = nil
+			n.wildChild = false
+			n.fullPath = fullPath[:parentFullPathIndex+i]
+		}
+
+		// 加入path的非公共前缀部分作为新的子结点
+		if i < len(path) {
+			path = path[i:]
+			c := path[0]
+
+			// TODO:
+			if n.nType == param && c == '/' && len(n.children) == 1 {
+				parentFullPathIndex += len(n.path)
+				n = n.children[0]
+				n.priority++
+				continue walk
+			}
+
+		}
+	}
+}
+
+func (n *node) insertChild(path string, fullPath string, handlers HandlersChain) {
+	// TODO:
+
+	n.path = path
+	n.fullPath = fullPath
+	n.handlers = handlers
 }
 
 type nodeValue struct {
